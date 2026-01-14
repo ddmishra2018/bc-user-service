2af7f5ccdaedbac605787a0ce954137cd8587d54
Dharani Dhara Mishra <dd_mishra@yahoo.com>
2026-01-14 15:47:40 +0530

    Added cors configuration and also spring profiles so that we can test the application loacally or in dev environment

---
+++ src/main/java/com/dezavoo/bootcamp/userservice/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.dezavoo.bootcamp.userservice.config;
+import java.util.List;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final List<String> allowedOrigins;
+    
+    public CorsConfig(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.toArray(String[]::new))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+}
