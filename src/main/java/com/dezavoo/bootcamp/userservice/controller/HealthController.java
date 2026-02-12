a354398794c4ec68554e38c4c1b99e3d94e720ff
Dharani Dhara Mishra <dd_mishra@yahoo.com>
2026-02-12 23:31:50 +0530

    Added healthcheck endpoint

---
+++ src/main/java/com/dezavoo/bootcamp/userservice/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.dezavoo.bootcamp.userservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Service is healthy");
+    }
+}
